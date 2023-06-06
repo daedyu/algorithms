75161e650041e0e18d0d9d45691a9156bb4a8fc4
daedyu <ceodaedyu@gmail.com>
2023-06-06 21:56:33 +0900

    correction commit

--- src/TEST/TEST011.java
+++ src/TEST/TEST011.java
@@ -17,37 +17,50 @@ import java.util.Stack;
 
 public class TEST011
 {
-     static Stack reverse(Stack<Integer> s)
-    {
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = s.toArray().length - 1; i >= 0; i--)
-        {
-            Object[] arr = s.toArray();
-            stack.push((Integer) arr[i]);
-        }
-        return stack;
-    }
     public static void main(String[] args)
     {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
 
-        Stack<Integer> integerStack = new Stack<>();
-        Stack<String> stringStack = new Stack<>();
-
-        int n = scan.nextInt();
-
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < N; i++)
         {
-            int k = scan.nextInt();
-            integerStack.push(k);
+            A[i] = sc.nextInt();
         }
 
-        new int
-
-        for (int i = 0; i < n; i++)
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+        int num = 1;
+        boolean result = true;
+        for (int i = 0; i < A.length; i++)
         {
+            int su = A[i];
+            if (su > num)
+            {
+                while (su >= num)
+                {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            }
+            else
+            {
+                int n = stack.pop();
 
+                if (n > su)
+                {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    bf.append("-\n");
+                }
+            }
         }
+        if (result) System.out.println(bf.toString());
     }
 }
