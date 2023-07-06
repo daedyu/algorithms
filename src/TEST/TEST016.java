1b4252328e5f1b13fc283a944e748d3176338b40
daedyu <ceodaedyu@gmail.com>
2023-07-06 22:56:35 +0900

    first commit

--- src/TEST/TEST016.java
+++
@@ -1,9 +0,0 @@
-package TEST;
-
-public class TEST016
-{
-    public static void main(String[] args)
-    {
-
-    }
-}
---
+++ src/TEST/TEST017.java
@@ -0,0 +1,24 @@
+package TEST;
+
+import java.util.*;
+
+public class TEST017
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        String N = scan.next();
+
+        int[] arr = new int[N.length()];
+
+        for (int i = 0; i < N.length(); i++)
+        {
+            arr[i] = Integer.parseInt(N.substring(i, i + 1));
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr);
+    }
+}
