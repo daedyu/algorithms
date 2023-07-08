76b08a9bf8a5cfda302c2463857d3945acfa19c6
daedyu <ceodaedyu@gmail.com>
2023-07-08 17:03:33 +0900

    second commit

--- src/TEST/TEST024.java
+++ src/TEST/TEST024.java
@@ -12,6 +12,51 @@ import java.util.Scanner;
 
 public class TEST024
 {
-    Scanner scan = new Scanner(System.in);
-    int N = scan.nextInt();
+    public static int N;
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+    }
+
+    public static boolean prime_number(int num)
+    {
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void dfs(int num, int digit)
+    {
+        if (digit == N)
+        {
+            if (prime_number(num))
+            {
+                System.out.println(num);
+            }
+            return;
+        }
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (i  % 2 == 0)
+            {
+                continue;
+            }
+            if (prime_number(num * 10 + i))
+            {
+                dfs(num * 10 + i, digit + 1);
+            }
+        }
+    }
 }
