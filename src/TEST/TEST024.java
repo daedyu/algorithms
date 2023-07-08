5bef394e3abcf3be0b7d86b00aad2dfec0c4eaef
daedyu <ceodaedyu@gmail.com>
2023-07-08 16:39:27 +0900

    first commit

---
+++ src/TEST/TEST024.java
@@ -0,0 +1,17 @@
+package TEST;
+
+/*
+신기한 소수 찾기
+    > 가장 관심있는 소수 = 7331 -> 733도 소수 73 도 소수 7또한 소수
+
+    stdin
+        > 첫번째 줄에 자릿수 N이 주어짐
+ */
+
+import java.util.Scanner;
+
+public class TEST024
+{
+    Scanner scan = new Scanner(System.in);
+    int N = scan.nextInt();
+}
