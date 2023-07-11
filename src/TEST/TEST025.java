50daaaff789f341e3f0002152e1b9a58087697b0
daedyu <ceodaedyu@gmail.com>
2023-07-11 21:51:26 +0900

    second commit

---
+++ src/TEST/TEST025.java
@@ -0,0 +1,16 @@
+package TEST;
+
+/*
+친구 관계 파익하기
+
+ */
+
+import java.util.LinkedList;
+
+public class TEST025
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+    }
+}
