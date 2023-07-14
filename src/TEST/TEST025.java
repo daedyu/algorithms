ec72d0d276aa62c3488ecb8bcfa45ae8de5d5d84
daedyu <ceodaedyu@gmail.com>
2023-07-14 16:51:24 +0900

    second commit

--- src/TEST/TEST025.java
+++ src/TEST/TEST025.java
@@ -1,16 +1,80 @@
 package TEST;
 
-/*
-친구 관계 파익하기
-
- */
-
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class TEST025
 {
+    static List<List<Integer>> lists;
+
     public static void main(String[] args)
     {
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+
+        lists = new ArrayList<>(N);
+        for (int i = 0; i < N; i++)
+        {
+            lists.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++)
+        {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            lists.get(a).add(b);
+            lists.get(b).add(a);
+        }
+
+        if (checkFriend())
+        {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    public static boolean checkFriend()
+    {
+
+        int[] num = {0, 1, 2, 3, 4};
+
+
+        for (int i = 0; i < num.length; i++)
+        {
+            boolean[] List_bool = new boolean[lists.size()];
+            int start = num[i];
+
+            List_bool[start] = true;
+
+            for (int friend : lists.get(start))
+            {
+                List_bool[friend] = true;
+
+                for (int friendOfFriend : lists.get(friend))
+                {
+                    List_bool[friendOfFriend] = true;
+                }
+            }
+
+            boolean Friends = true;
+            for (int j = 0; j < num.length; j++)
+            {
+                if (!List_bool[num[j]])
+                {
+                    Friends = false;
+                    break;
+                }
+            }
+
+            if (Friends)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
